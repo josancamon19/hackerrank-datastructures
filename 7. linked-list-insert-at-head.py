445c2f8cb9dd3dd1a8f503302a4c9123e3b1ef2a
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-11-07 21:26:23 -0500

    Linked List insert at head problem

---
+++ 7. linked-list-insert-at-head.py
@@ -0,0 +1,10 @@
+# def insertNodeAtHead(llist, data):
+#     new_node = SinglyLinkedListNode(data)
+#     # Write your code here
+#     if llist is None:
+#         llist = new_node
+#     else:
+#         new_node.next = llist
+#         llist = new_node
+#
+#     return llist
